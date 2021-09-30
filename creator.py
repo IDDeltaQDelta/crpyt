15b96aecd8bdb3b95e0329246e6c3897398d6381
IDDeltaQDelta <iddeltaqdelta@gmail.com>
2021-09-30 17:21:22 +0300

    create first images

--- creator.py
+++ creator.py
@@ -5,21 +5,26 @@ from colors import randomColor
 
 
 def imageCreator():
-    img = Image.open("SZE3.png") # img contain image. should contain path to image
+    a = randomColor()
+    b = randomColor()
+    c = randomColor()
+    d = randomColor()
+    e = randomColor()
+    img = Image.open("image/SZE3.png") # img contain image. should contain path to image
     pixels = img.load()  # create the pixel map
     for i in range(img.size[0]): # for every pixel:
         # Если добавить цвета в эту строчку то будет радужная картинка для каждой вертикальной линии
         for j in range(img.size[1]):
             if pixels[i,j] == (0,0,0): # black #000000
-                pixels[i,j] = randomColor()
+                pixels[i,j] = a
             if pixels[i,j] == (255, 255, 255): # white
-                pixels[i, j] = randomColor()
+                pixels[i, j] = b
             if pixels[i,j] == (0, 36, 255): # red #ff0000
-                pixels[i, j] = randomColor()
+                pixels[i, j] = c
             if pixels[i,j] == (246, 0, 255): # green
-                pixels[i, j] = randomColor()
+                pixels[i, j] = d
             if pixels[i,j] == (255, 234, 0): # green
-                pixels[i, j] = randomColor()
+                pixels[i, j] = e
     return img.show()
 
 
